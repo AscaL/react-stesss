8992b51f6ac33cc4001534c0aa6efb9eac0d8952
AscaL <emanueleorfanelli@gmail.com>
2016-03-04 13:03:07 +0100

    class should be working

--- src/app/index.js
+++ src/app/index.js
@@ -3,17 +3,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class HelloWorld extends React.Component {
-  displayName() {
-    return "hello message"
-  }
+const USER_DATA = {
+  name: 'Brollo',
+  username: 'AscaL',
+  image: 'https://avatars3.githubusercontent.com/u/8880943?v=3&u=9ddb1925dc47817cd05101412f92fb9cfc3ddcf2&s=140'
+}
+
+class ProfilePic extends React.Compontent {
   render() {
     return (
-    < div >
-      Hello {this.props.name}!
-    < /div>
+      <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
     )
   }
-};
+}
 
-ReactDOM.render( < HelloWorld name="yo" / > , document.getElementById('app'));
+class ProfileLink extends React.Component {
+  render() {
+    return (
+      <div>
+        <a href={'http://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+}
+
+class ProfileName extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.name}
+      </div>
+    )
+  }
+}
+
+class Avatar extends React.Component {
+  render() {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render (
+  < Avatar user={USER_DATA} />,
+  document.getElementById('app')
+);
